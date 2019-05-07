e976bdb0b7630b24061211519377fc33ea10003f
triamp.vic <yl.vic.liu@gmail.com>
2019-05-07 13:07:20 +0800

    video player init

---
+++ Video-Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video-Player
+//
+//  Created by vic_liu on 2019/5/7.
+//  Copyright © 2019 ios-class. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
