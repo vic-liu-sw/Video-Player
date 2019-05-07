ceb1d5cc6105b6d18137cafa9110f98338aacc2b
triamp.vic <yl.vic.liu@gmail.com>
2019-05-07 17:20:33 +0800

    AVplayer ok

--- Video-Player/ViewController.swift
+++ Video-Player/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
     var searchBar: UISearchBar!
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
     var playButton:UIButton!
     var pauseButton:UIButton!
     var serrchController: UISearchController!
+    var newM3u8: String?
 
+    var playerItem: AVPlayerItem!
+    var playerLayer: AVPlayerLayer!
+    var player: AVPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,8 +70,74 @@ class ViewController: UIViewController {
         pauseButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         pauseButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
 
+        cancelButton.addTarget(self, action: #selector(ViewController.cancel(_:)), for: .touchUpInside)
+        playButton.addTarget(self, action: #selector(ViewController.play(_:)), for: .touchUpInside)
+        pauseButton.addTarget(self, action: #selector(ViewController.pause(_:)), for: .touchUpInside)
+
+        playerItem = M3u8ResourceLoader.shared.playerItem(with: "")
+
+        player = AVPlayer(playerItem: playerItem)
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resizeAspect
+        playerLayer.contentsScale = UIScreen.main.scale
+        playerLayer.frame = UIScreen.main.bounds
+
     }
 
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(AVPlayerItem.loadedTimeRanges) {
+
+        } else if keyPath == #keyPath(AVPlayerItem.status) {
+
+            if playerItem.status == .readyToPlay {
+
+                player.play()
+            } else {
+                print("loading error")
+            }
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+
+    deinit {
+        playerItem.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.status))
+        playerItem.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.loadedTimeRanges))
+    }
+
+    @objc func cancel(_ sender: UIButton) {
+
+        searchBar.text = ""
+    }
+
+    @objc func play(_ sender: UIButton) {
+
+        var sampleM3u8 = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"
+        newM3u8 = avplayString(m3u8: sampleM3u8)
+        searchBar.text = newM3u8
+        view.layer.insertSublayer(playerLayer, at: 0)
+
+        if sender.isEnabled {
+            player.play()
+        }
+
+        playerItem.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: .new, context: nil)
+        playerItem.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.loadedTimeRanges), options: .new, context: nil)
+
+
+    }
+
+    @objc func pause(_ sender: UIButton) {
+        player.pause()
+    }
+
+    func avplayString(m3u8:String) -> String {
+
+        return m3u8
+
+    }
 
 }
+
+
 
